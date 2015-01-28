4395c3b23cceab9fdf17082e45feb5ac0b932575
missionkao <kaominghsuan@gmail.com>
2015-01-29 00:33:35 +0800

    modify some css and background image

--- javascripts/chart.js
+++ javascripts/chart.js
@@ -9,10 +9,11 @@ $(function () {
         },
         
         title: {
-            text: 'Technology Skills'
+            text: 'Technology Skills / Languages'
         },
         xAxis: {
-            categories: ['Matlab', 'HTML5/CSS', 'Javascript', 'Java','C/C++', 'Android','IOS' ,'Verilog']
+            categories: ['Matlab', 'HTML5/CSS', 'Javascript', 'Java','C/C++', 'Android','IOS' ,'Verilog', 
+                            'Mandarin', 'Taiwanese', 'English', 'Japanese']
         },
         yAxis: {
             title: {
@@ -27,43 +28,43 @@ $(function () {
         },
         series: [{
             name: 'Terry Kao',
-            data: [90, 70, 60, 60, 50, 30, 0, 10 ]
+            data: [90, 70, 60, 60, 50, 30, 0, 10, 100, 90, 60, 30 ]
         }]
     });
 });
 
 
 
-$(function () { 
+// $(function () { 
 
-        Highcharts.setOptions({
-        colors: ['#058DC7']
-    });
-    $('#language').highcharts({
-        chart: {
-            type: 'bar'
-        },
+//         Highcharts.setOptions({
+//         colors: ['#058DC7']
+//     });
+//     $('#language').highcharts({
+//         chart: {
+//             type: 'bar'
+//         },
         
-        title: {
-            text: 'Languages'
-        },
-        xAxis: {
-            categories: ['Mandarin', 'Taiwanese', 'English', 'Japanese','Spanish']
-        },
-        yAxis: {
-            title: {
-                text: 'Percentage (%)'
-            },
-            max: 100
-        },
-        plotOptions: {
-            series: {
-                borderColor: '#303030'
-            }
-        },
-        series: [{
-            name: 'Terry Kao',
-            data: [100, 90, 60, 30, 5]
-        }]
-    });
-});
+//         title: {
+//             text: 'Languages'
+//         },
+//         xAxis: {
+//             categories: ['Mandarin', 'Taiwanese', 'English', 'Japanese','Spanish']
+//         },
+//         yAxis: {
+//             title: {
+//                 text: 'Percentage (%)'
+//             },
+//             max: 100
+//         },
+//         plotOptions: {
+//             series: {
+//                 borderColor: '#303030'
+//             }
+//         },
+//         series: [{
+//             name: 'Terry Kao',
+//             data: [100, 90, 60, 30, 5]
+//         }]
+//     });
+// });
